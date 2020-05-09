e97d497010310cd7a8fa96b8ef563b235b779aa1
furtadobb <furtadobb@gmail.com>
2020-05-09 15:39:49 -0300

    Qualification generation

--- home/input/generator.py
+++ home/input/generator.py
@@ -1,27 +1,24 @@
 import pandas as pd
 
-
-# Load qualifications data 2000, combining municipal-level with AP-level
-quali = pd.read_csv('home/input/qualification_2000.csv', sep=';')
-
-# rename from cod_mun b/c we may also have
-# AP codes, not just municipal codes
-quali.rename(columns={'cod_mun': 'code'}, inplace=True)
-quali_aps = pd.read_csv('home/input/qualification_APs.csv', sep=';', header=0,
-                      decimal=',').apply(pd.to_numeric, errors='coerce')
-for code, group in quali_aps.groupby('AREAP'):
-    group = group[['qual', 'perc_qual_AP']]
-    row = {'code': code}
-    for idx, qual, percent in group.to_records():
-        row[str(qual)] = percent
-    row = [row.get(col, 0) for col in quali.columns]
-    quali.loc[quali.shape[0]] = row
-quali.set_index('code', inplace=True)
-quali_sum = quali.cumsum(axis=1)
+import home.input.geography as geo
 
 
-single_ap_muns = pd.read_csv('input/single_aps.csv', sep=';')
-single_ap_muns = single_ap_muns['mun_code'].tolist()
+def quali_table(metro):
+    """ Provide list of municipality codes for a metropolitan region and
+    return a table with APs codes and percentage of qualification by years of study
+    """
+    params = dict()
+    params['PROCESSING_ACPS'] = [metro]
+    my_geo = geo.Geography(params)
+    mun_codes = [str(value) for value in my_geo.mun_codes]
+    # Load qualifications data 2000, combining municipal-level with AP-level
+    quali_aps = pd.read_csv('quali_aps.csv', sep=';')
+    quali_aps.AREAP = quali_aps.AREAP.astype(str)
+    selected_quali = quali_aps[quali_aps.AREAP.str[:7].isin(mun_codes)]
+    return selected_quali
+
 
 if __name__ == '__main__':
-    pass
+    metropolitan_region_of_choice = 'BRASILIA'
+    qt = quali_table(metropolitan_region_of_choice)
+
