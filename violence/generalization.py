b7e51b3875eacfbd0873af6c7d017ca732f601c9
bafurtado <furtadobb@gmail.com>
2020-05-17 20:01:31 -0300

    BatchRunner. Fantastic. Thanks mesa developers (@JackieKazil @badnetowrker)

--- violence/generalization.py
+++ violence/generalization.py
@@ -40,13 +40,15 @@ def main(parameters, iterations=50):
 
 if __name__ == '__main__':
     subdivisions = 2
+    # Remember to use BatchRunnerMP
+    # Remember to save figures and database
     params = {'gender_stress': np.linspace(.1, .9, subdivisions),
-              'metro': metropolis,
               'under_influence': np.linspace(.01, .5, subdivisions),
               'has_gun': np.linspace(.1, .9, subdivisions),
               'is_working_pct': np.linspace(.1, .9, subdivisions),
               'chance_changing_working_status': np.linspace(.01, .5, subdivisions),
               'pct_change_wage': np.linspace(.01, .5, subdivisions)}
+              #'metro': metropolis}
     df = main(params, iterations=2)
     df.loc[:, 'aggressor_pct'] = df['Aggressor'] / df['Person']
     for each in params.keys():
