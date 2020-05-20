f3803a8ca6344ecf64341a7ad912a169c0361779
bafurtado <furtadobb@gmail.com>
2020-05-20 15:43:01 -0300

    metropolis plot

--- violence/plotting.py
+++ violence/plotting.py
@@ -50,7 +50,7 @@ def set_proper_ticks(ax, x_label='', y_label='', title='', legend=''):
     ax.spines['left'].set_visible(False)
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
-    ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.5f}'.format))
+    ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.4f}'.format))
     ax.legend = ([legend])
     return ax
 
@@ -65,14 +65,18 @@ def plot(data, group_col, plot_col):
 
 
 def another_plot(data, col_interest, col_aggregate):
-    # TODO: Border, Legend, Abbvr. Ticks
     fig, ax = plt.subplots()
+    ax = set_proper_ticks(ax)
     data = data.groupby(by=col_aggregate).agg('median').reset_index()
     data = data.sort_values(by=[col_interest])
-    data[col_interest].plot(kind='bar', legend=col_aggregate, ylim=[min(data[col_interest]), max(data[col_interest])])
-    sns.palplot(sns.color_palette("Blues"))
-    fig = sns.barplot(data=data, x=col_aggregate, y=col_interest)
-    fig.plot()
+
+    ax = sns.barplot(x="metro", y="aggressor_pct", data=data, palette="Blues_d", ax=ax)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90, horizontalalignment='right')
+    ax.axes.set_title("Percentage of aggressors by metropolis", fontsize=10)
+    ax.set_xlabel("metropolis", fontsize=10)
+    ax.set_ylabel("Perc. Aggressors", fontsize=8)
+    ax.tick_params(labelsize=6)
+    plt.ylim(min(data[col_interest]) - .0005, max(data[col_interest]))
     plt.show()
 
 
@@ -80,7 +84,7 @@ if __name__ == '__main__':
     # df3 = pd.read_csv('output/bundled_together.csv', sep=';')
     # plot(df3, 'gender_stress', "aggressor_pct")
 
-    # df = pd.read_csv('output/output_metropolis.csv', sep=';')
-    # another_plot(df, 'aggressor_pct', 'metro')
+    df = pd.read_csv("output/output_200_8_dict_keys(['metro']).csv", sep=';')
+    another_plot(df, 'aggressor_pct', 'metro')
 
-    summary()
+    # summary()
